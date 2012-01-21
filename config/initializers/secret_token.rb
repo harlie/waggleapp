acb90acd1d183e1370f295c4cb5d5a8559999b6c
Harlie Levine <harlie.levine@gmail.com>
2012-01-21 12:12:36 -0500

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Waggle::Application.config.secret_token = '0863f0b192b93c2a9de8ba6691d2992601ffe8bfe5ca120162a61c1c2039da5efb1abd6813995f01b8e60f225374883e7f2340aae8bad8f2cede10ca78fe3a78'
