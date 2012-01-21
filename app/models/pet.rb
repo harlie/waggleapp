5ee952f70855f31492049c0e98c93458a1a65339
harlie.levine@gmail.com <harlie.levine@gmail.com>
2012-01-21 17:16:00 -0500

    adding some more fields to pets

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,10 +1,12 @@
 class Pet
   include Mongoid::Document
   include Mongoid::Timestamps
-  
+  include Mongoid::MultiParameterAttributes
   
   field :name, :type => String
   field :birthdate, :type => Date
+  field :breed, :type => String
+  field :gender, :type => String
   
   belongs_to :user
   embeds_many :updates
